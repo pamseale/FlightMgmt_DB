44ba7d4498ad3e8d04cce0f0a95ffccd08f2264f
pamseale <pamelamseale@gmail.com>
2025-04-24 22:12:43 +0100

    tidy up col names

--- db_setup.sql
+++ db_setup.sql
@@ -1,19 +1,29 @@
-DROP TABLE IF EXISTS staff;
+DROP TABLE IF EXISTS flight;
 DROP TABLE IF EXISTS airport;
 DROP TABLE IF EXISTS airport_status;
 DROP TABLE IF EXISTS plane;
-DROP TABLE IF EXISTS flight;
+DROP TABLE IF EXISTS staff;
 
 DROP VIEW IF EXISTS view_all_flights;
-DROP VIEW IF EXISTS view_all_staff;
 DROP VIEW IF EXISTS view_all_airports;
+DROP VIEW IF EXISTS view_all_staff;
 
 
-CREATE TABLE IF NOT EXISTS staff (
+CREATE TABLE IF NOT EXISTS flight (
     ID VARCHAR(20) NOT NULL,
-    surname VARCHAR(255) NOT NULL,
-    forename VARCHAR(255) NOT NULL,
-    role VARCHAR(255) NOT NULL,
+    IATA_flight_no VARCHAR(20) NOT NULL,
+    plane_ID VARCHAR(20) NOT NULL,
+    airport_dep_ID VARCHAR(3) NOT NULL,
+    airport_arr_ID VARCHAR(3) NOT NULL,
+    pilot_ID VARCHAR(20) NOT NULL,
+    first_officer_ID VARCHAR(20),
+    date_dep_scheduled DATETIME NOT NULL,
+    date_arr_scheduled DATETIME NOT NULL,
+    FOREIGN KEY (plane_ID) REFERENCES plane(ID),
+    FOREIGN KEY (airport_dep_ID) REFERENCES airport(ID),
+    FOREIGN KEY (airport_arr_ID) REFERENCES airport(ID),
+    FOREIGN KEY (pilot_ID) REFERENCES staff(ID),
+    FOREIGN KEY (first_officer_ID) REFERENCES staff(ID),
     PRIMARY KEY (ID)
     );
 
@@ -24,8 +34,8 @@ CREATE TABLE IF NOT EXISTS airport (
     city VARCHAR(255) NOT NULL,
     country VARCHAR(255) NOT NULL,
     continent VARCHAR(255) NOT NULL,
-    airport_status INT NOT NULL,
-    FOREIGN KEY (airport_status) REFERENCES airport(ID),
+    status INT NOT NULL,
+    FOREIGN KEY (status) REFERENCES airport(ID),
     PRIMARY KEY (ID)
     );
 
@@ -46,36 +56,19 @@ CREATE TABLE IF NOT EXISTS plane (
     PRIMARY KEY (ID)
     );
 
-CREATE TABLE IF NOT EXISTS flight (
+CREATE TABLE IF NOT EXISTS staff (
     ID VARCHAR(20) NOT NULL,
-    IATA_flight_no VARCHAR(20) NOT NULL,
-    plane_ID VARCHAR(20) NOT NULL,
-    airport_dep_ID VARCHAR(3) NOT NULL,
-    airport_arr_ID VARCHAR(3) NOT NULL,
-    pilot_ID VARCHAR(20) NOT NULL,
-    first_officer_ID VARCHAR(20),
-    date_dep_scheduled DATETIME NOT NULL,
-    date_arr_scheduled DATETIME NOT NULL,
-    FOREIGN KEY (plane_ID) REFERENCES plane(ID),
-    FOREIGN KEY (airport_dep_ID) REFERENCES airport(ID),
-    FOREIGN KEY (airport_arr_ID) REFERENCES airport(ID),
-    FOREIGN KEY (pilot_ID) REFERENCES staff(ID),
-    FOREIGN KEY (first_officer_ID) REFERENCES staff(ID),
+    surname VARCHAR(255) NOT NULL,
+    forename VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
     PRIMARY KEY (ID)
     );
 
 
-INSERT INTO staff VALUES ('STID001', 'Lancaster', 'Timothy', 'pilot');
-INSERT INTO staff VALUES ('STID002', 'Aitchison', 'Alastair', 'pilot');
-INSERT INTO staff VALUES ('STID003', 'Ogden', 'Nigel', 'cabin crew');
-INSERT INTO staff VALUES ('STID004', 'Dubois', 'Marc', 'pilot');
-INSERT INTO staff VALUES ('STID005', 'Robert', 'David', 'pilot');
-INSERT INTO staff VALUES ('STID006', 'Bonin', 'Pierre-Cedric', 'pilot');
-INSERT INTO staff VALUES ('STID007', 'Danilov', 'Andrey', 'pilot');
-INSERT INTO staff VALUES ('STID008', 'Piskaryov', 'Igor', 'pilot');
-INSERT INTO staff VALUES ('STID009', 'Kudrinsky', 'Yaroslav', 'pilot');
-INSERT INTO staff VALUES ('STID010', 'Sullenberger', 'Chelsey', 'pilot');
-INSERT INTO staff VALUES ('STID011', 'Skiles', 'Jeffrey', 'pilot');
+INSERT INTO flight VALUES ('FLID001', 'BA5390', 'PLID001', 'BIR', 'MAL', 'STID001', 'STID002', '1990-06-10 07:20:00', '1990-06-10 10:15:00');
+INSERT INTO flight VALUES ('FLID002', 'AFR447', 'PLID002', 'GIG', 'CDG', 'STID004', 'STID005', '2009-05-31 22:29:00', '2009-05-31 09:03:00');
+INSERT INTO flight VALUES ('FLID003', 'AFL593', 'PLID003', 'SVO', 'HKG', 'STID007', 'STID008', '1994-03-22 10:39:00', '1994-05-31 00:46:00');
+INSERT INTO flight VALUES ('FLID004', 'US1549', 'PLID004', 'LGA', 'SEA', 'STID010', 'STID011', '2009-01-15 20:24:56', '2009-02-15 01:44:00');
 
 INSERT INTO airport VALUES ('APID001', 'BIR', 'Birmingham Airport', 'Birmingham', 'UK', 'Europe', 1);
 INSERT INTO airport VALUES ('APID002', 'MAL', 'Malaga Airport', 'Malaga', 'Spain', 'Europe', 1);
@@ -95,10 +88,18 @@ INSERT INTO plane VALUES ('PLID002', 'F-GZCP', 'Airbus A330-203', 2005, 3, 9, 21
 INSERT INTO plane VALUES ('PLID003', 'F-OGQS', 'Airbus A310-304', 1992, 3, 9, 63);
 INSERT INTO plane VALUES ('PLID004', 'N106US', 'Airbus A320-214', 1984, 2, 3, 150);
 
-INSERT INTO flight VALUES ('FLID001', 'BA5390', 'PLID001', 'BIR', 'MAL', 'STID001', 'STID002', '1990-06-10 07:20:00', '1990-06-10 10:15:00');
-INSERT INTO flight VALUES ('FLID002', 'AFR447', 'PLID002', 'GIG', 'CDG', 'STID004', 'STID005', '2009-05-31 22:29:00', '2009-05-31 09:03:00');
-INSERT INTO flight VALUES ('FLID003', 'AFL593', 'PLID003', 'SVO', 'HKG', 'STID007', 'STID008', '1994-03-22 10:39:00', '1994-05-31 00:46:00');
-INSERT INTO flight VALUES ('FLID004', 'US1549', 'PLID004', 'LGA', 'SEA', 'STID010', 'STID011', '2009-01-15 20:24:56', '2009-02-15 01:44:00');
+INSERT INTO staff VALUES ('STID001', 'Lancaster', 'Timothy', 'pilot');
+INSERT INTO staff VALUES ('STID002', 'Aitchison', 'Alastair', 'pilot');
+INSERT INTO staff VALUES ('STID003', 'Ogden', 'Nigel', 'cabin crew');
+INSERT INTO staff VALUES ('STID004', 'Dubois', 'Marc', 'pilot');
+INSERT INTO staff VALUES ('STID005', 'Robert', 'David', 'pilot');
+INSERT INTO staff VALUES ('STID006', 'Bonin', 'Pierre-Cedric', 'pilot');
+INSERT INTO staff VALUES ('STID007', 'Danilov', 'Andrey', 'pilot');
+INSERT INTO staff VALUES ('STID008', 'Piskaryov', 'Igor', 'pilot');
+INSERT INTO staff VALUES ('STID009', 'Kudrinsky', 'Yaroslav', 'pilot');
+INSERT INTO staff VALUES ('STID010', 'Sullenberger', 'Chelsey', 'pilot');
+INSERT INTO staff VALUES ('STID011', 'Skiles', 'Jeffrey', 'pilot');
+
 
 CREATE VIEW IF NOT EXISTS view_all_flights AS
 SELECT 
@@ -126,7 +127,7 @@ SELECT
     a.continent,
     st.status_desc
 FROM airport as a
-LEFT JOIN airport_status AS st ON st.ID=a.airport_status;
+LEFT JOIN airport_status AS st ON st.ID=a.status;
 
 CREATE VIEW IF NOT EXISTS view_all_staff AS
 SELECT 
