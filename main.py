186a6cfae26a3a0a2219697fda8d2a5976cc2dbc
pamseale <pamelamseale@gmail.com>
2025-04-25 02:30:55 +0100

    security added

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from file_handler import execute_sql_file
 from menu_functions import welcome, start_menu, execute_menu_choice
+from security import login
 
 db_filename = 'flight_management_database.db'
 db_setup_filename = 'db_setup.sql'
 
 def main():
+
+   verified_user = False
+   while(verified_user == False):
+      verified_user = login()
+
    execute_sql_file(db_setup_filename, db_filename)
    welcome()
    run = True
---
+++ security.py
@@ -0,0 +1,17 @@
+valid_user = "user1"
+valid_password = "1234!"
+
+def login():
+        print("Please log in to access the Flight Manager:\n")
+        supplied_username = input("Username: ")
+        supplied_password = input("Password: ")
+        if supplied_username == valid_user and supplied_password == valid_password:
+            print("login details accepted.")
+            return True
+        else:
+            response = input("login failed. Press any key to try again or 'E' to exit.")
+            if response == "E":
+                exit()
+            else:
+                return False
+
